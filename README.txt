d22b93187883c63da42ba02274f7404c0eb10bcc
Andreas Stefik <stefika@stefik02.cs.unlv.edu>
2014-08-06 15:06:39 -0700

    Added the latest version of the source from sourceforge.

---
+++ README.txt
@@ -0,0 +1,11 @@
+= Phonemic version 1.5, June 26, 2012 =
+
+== Introduction ==
+This is the third official release of the Phonemic speech library. Starting with this release, Phonemic now has the ability to run as a daemon speech server, handling multiple client connections over a TCP protocol. 
+
+This new feature allows multiple programs to use the same speech settings at the same time, preventing confusion with multiple voices and cross-talk.
+
+The TCP protocol used by Phonemic in this release is documented at the following Trac page: https://sourceforge.net/apps/trac/phonemic/wiki/PhonemicProtocol
+
+== More Information ==
+This release is primarily an enhancement release. Build notes and previous version information can be found at https://sourceforge.net/apps/trac/phonemic/wiki/ReleaseNotes
---
+++ example/phonemicTestApp/src/phonemictestapp/PhonemicTestApp.java
@@ -0,0 +1,155 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package phonemictestapp;
+
+import java.util.Iterator;
+import org.sodbeans.phonemic.*;
+import org.sodbeans.phonemic.tts.*;
+
+/**
+ *
+ * @author jeff
+ */
+public class PhonemicTestApp {
+
+    private static String introText = "This is a simple application using the "
+            + " Phonemic speech library. I will read any text you pass as a command line argument. "
+            + " Since you didn't pass me a "
+            + " command line argument, I am just speaking this introductory text."
+            + " This application demonstrates how to get started quickly with Phonemic."
+            + " To get started using Phonemic with your own project, include the phonemic.jar"
+            + " in your project with the required library files.";
+    private static final String longSampleText = introText + " Once again; " + introText;
+            
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Get a text-to-speech object from Phonemic.
+        TextToSpeech speech = TextToSpeechFactory.getDefaultTextToSpeech();
+
+        // We will either speak a little bit of introductory text, or we will
+        // speak the first command line argument.
+        if (args.length > 0) {
+            speech.speakBlocking(args[0]);
+        } else {
+            // Default engine should be JAWS if it's installed.
+            System.out.println("If JAWS is installed, it should be the default engine");
+            System.out.println("Current engine: " + speech.getTextToSpeechEngine().getEngineName());
+
+            if (speech.isSpeaking()) {
+                System.out.println("PROBLEM: ENGINE THINKS IT'S SPEAKING WHEN IT ISN'T");
+            }
+            speech.speak("Basic speech should work");
+
+            (new Thread(new SpeechKiller(5000))).start();
+            speech.speak("Unblocking speech should be killable. " + longSampleText);
+
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                // No big deal.
+            }
+
+            if (!speech.isSpeaking()) {
+                System.out.println("PROBLEM: ENGINE THINKS IT'S NOT SPEAKING WHEN IT IS");
+            }
+
+            while (speech.isSpeaking()) {
+                ;
+            }
+
+            (new Thread(new SpeechKiller(1000))).start();
+            speech.speakBlocking("Blocking speech should not be killable. Surely "
+                    + "these two sentences will take at least 2 seconds to say.");
+
+            Iterator<SpeechVoice> voices = speech.getAvailableVoices();
+            int voiceCount = 0;
+
+            while (voices.hasNext()) {
+                SpeechVoice voice = voices.next();
+                voiceCount++;
+
+                if (!speech.setVoice(voice)) {
+                    System.out.println("PROBLEM: COULDN'T SET THE ENGINE'S VOICE");
+                }
+
+                if (!speech.getCurrentVoice().getName().contains(voice.getName())) {
+                    System.out.println("PROBLEM: COULDN'T GET THE ENGINE'S VOICE");
+                }
+            }
+
+            if (voiceCount == 0) {
+                System.out.println("PROBLEM: NO VOICES FOUND");
+            }
+
+            speech.speak("This text will pause at some point for 1 second. And then it will continue. And then, later, it will stop. " + longSampleText);
+
+            //Should pause
+            speech.pause();
+
+            // Should resume
+            speech.resume();
+
+            //Should stop
+            speech.stop();
+
+            double newSpeed = 3.0;
+
+            // Should set the speed.
+            speech.setSpeed(newSpeed);
+
+            // Should set the volume
+            double newVolume = 0.8;
+            speech.setVolume(newVolume);
+
+            speech.reinitialize();
+            speech.speak("Should still work after reinitialising.");
+
+            if (!speech.setTextToSpeechEngine(TextToSpeechEngine.MICROSOFT_SAPI)) {
+                System.out.println("Could not set speech engine");
+            }
+            if (speech.getTextToSpeechEngine() != TextToSpeechEngine.MICROSOFT_SAPI) {
+                System.out.println("Could not get speech engine");
+            }
+
+            if (!speech.setTextToSpeechEngine(TextToSpeechEngine.JAWS)) {
+                System.out.println("Could not set speech engine");
+            }
+            if (speech.getTextToSpeechEngine() != TextToSpeechEngine.JAWS) {
+                System.out.println("Could not get speech engine");
+            }
+
+            if (!speech.setTextToSpeechEngine(TextToSpeechEngine.NVDA)) {
+                System.out.println("Could not set speech engine");
+            }
+            if (speech.getTextToSpeechEngine() != TextToSpeechEngine.NVDA) {
+                System.out.println("Could not get speech engine");
+            }
+        }
+
+        // Terminate all Phonemic threads by exiting.
+        System.exit(0);
+    }
+
+    private static class SpeechKiller implements Runnable {
+
+        private final long wait_ms;
+
+        public SpeechKiller(long wait_ms) {
+            this.wait_ms = wait_ms;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(wait_ms);
+            } catch (InterruptedException e) {
+                // No big deal
+            }
+            TextToSpeechFactory.getDefaultTextToSpeech().stop();
+        }
+    }
+}
